a1a3df674429aeae0c37287cd9768ceebf6cd3fc
snfjddl <seokil1220@gmail.com>
2022-01-18 23:10:37 +0900

    프로젝트 환경설정

---
+++ src/main/java/org/zerock/club/controller/SampleController.java
@@ -0,0 +1,34 @@
+package org.zerock.club.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/sample")
+public class SampleController {
+
+    // 로그인을 하지 않은 사용자도 접근 가능
+    @GetMapping("/all")
+    public void exAll() {
+        log.info("exAll........");
+    }
+
+    // 로그인한 사용자만 접근 가능
+    @GetMapping("/member")
+    public void exMember() {
+        log.info("exMember........");
+    }
+
+    // 관리자 권한이 있는 사용자만 접근 가능
+    @GetMapping("/admin")
+    public void exAdmin() {
+        log.info("exAdmin.........");
+    }
+
+}
+
+
+
