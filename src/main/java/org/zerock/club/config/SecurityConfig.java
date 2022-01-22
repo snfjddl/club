0bc6d1d3787261c9889a6669b3041b1feb0684d3
snfjddl <seokil1220@gmail.com>
2022-01-22 11:45:36 +0900

    회원 DB 생성 및 조회 테스트

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -1,11 +1,42 @@
 package org.zerock.club.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+
+        // Authorization 실패 시 로그인 화면
+        http.formLogin();
+        // csrf 토큰 비활성화
+        http.csrf().disable();
+        // 로그아웃 화면
+        http.logout();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user1")
+                .password("$2a$10$eZAd/mhPnCCMWB3FStz5C.pW7.5nyEAdbh0VdLiwsshCULNwHEtfu")
+                .roles("USER");
+    }
 }
---
+++ src/main/java/org/zerock/club/entity/ClubMember.java
@@ -0,0 +1,35 @@
+package org.zerock.club.entity;
+
+import lombok.*;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class ClubMember extends BaseEntity {
+
+    @Id
+    private String email;
+
+    private String password;
+    private String name;
+    private boolean fromSocial;
+
+    @ElementCollection(fetch = FetchType.LAZY)  // 객체의 일부로만 사용됨. 객체간 연관관계랑 약간 다른 동작?
+    @Builder.Default
+    private Set<ClubMemberRole> roleSet = new HashSet<>();
+
+    public void addMemberRole(ClubMemberRole clubMemberRole) {
+        roleSet.add(clubMemberRole);
+    }
+
+}
